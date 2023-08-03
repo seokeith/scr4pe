4ecb481f5525ac5097fcaf7d59ff8d33bf2d129e
seokeith <141214829+seokeith@users.noreply.github.com>
2023-08-03 16:08:20 +0100

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,29 +1,100 @@
-import streamlit as st
+# Import necessary libraries
 import requests
+import os
 from bs4 import BeautifulSoup
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+from nltk.collocations import BigramAssocMeasures, BigramCollocationFinder
+import newspaper
+from newspaper import Article
+import nltk
+import statistics
+import collections
+from nltk.collocations import TrigramAssocMeasures, TrigramCollocationFinder
+from nltk.collocations import QuadgramAssocMeasures, QuadgramCollocationFinder
+import time
+import openai
+import pandas as pd
 import re
+import streamlit as st
+from apify_client import ApifyClient
+import pandas as pd
+import transformers
+from transformers import GPT2Tokenizer
 
-def google_scrape(query):
-    # Replace with your custom Google Search URL
-    url = f"https://cse.google.com/cse?cx=152d311f722f0406c&q={query}"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    search_results = soup.find_all('div', class_='kCrYT')
-    urls = []
-    for result in search_results:
-        link = result.find('a', href=True)
-        if link:
-            raw_link = link['href']
-            clean_link = re.search('\/url\?q=(.*)\&sa', raw_link)
-            if clean_link:
-                urls.append(clean_link.group(1))
-    return urls
+import json
+#openai.api_key = openai.api_key = os.environ['openai_api_key']
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
-st.title('Google URL Scraper')
-search_term = st.text_input('Enter your search term:')
-if st.button('Scrape'):
-    if search_term:
-        urls = google_scrape(search_term)
-        st.write(urls)
-    else:
-        st.write('Please enter a search term.')
+
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('tagsets')
+nltk.download('words')
+nltk.download('maxent_ne_chunker')
+nltk.download('vader_lexicon')
+nltk.download('inaugural')
+nltk.download('webtext')
+nltk.download('treebank')
+nltk.download('gutenberg')
+nltk.download('genesis')
+nltk.download('trigram_collocations')
+nltk.download('quadgram_collocations')
+
+
+# Define a function to scrape Google search results and create a dataframe
+from apify_client import ApifyClient
+import pandas as pd
+import streamlit as st
+
+@st.cache_data(show_spinner=False)
+def scrape_google(search):
+    # Define the Apify API URL and the actor's name
+    APIFY_API_URL = 'https://api.apify.com/v2'
+    ACTOR_NAME = 'apify/google-search-scraper'
+
+    # Retrieve the Apify API key from Streamlit secrets
+    APIFY_API_KEY = st.secrets["APIFY_API_KEY"]
+
+    # Initialize the ApifyClient with your API token
+    client = ApifyClient(APIFY_API_KEY)
+
+    # Prepare the actor input
+    run_input = {
+        "csvFriendlyOutput": False,
+        "customDataFunction": "async ({ input, $, request, response, html }) => {\n  return {\n    pageTitle: $('title').text(),\n  };\n};",
+        "includeUnfilteredResults": False,
+        "maxPagesPerQuery": 1,
+        "mobileResults": False,
+        "queries": search,
+        "resultsPerPage": 10,
+        "saveHtml": False,
+        "saveHtmlToKeyValueStore": False
+    }
+
+    print(f"Running Google Search Scrape for {search}")
+    # Run the actor and wait for it to finish
+    run = client.actor(ACTOR_NAME).call(run_input=run_input)
+    print(f"Finished Google Search Scrape for {search}")
+
+    # Fetch the actor results from the run's dataset
+    results = []
+    for item in client.dataset(run["defaultDatasetId"]).iterate_items():
+        results.append(item)
+
+    # Extract URLs from organic results
+    organic_results = [item['organicResults'] for item in results]
+    urls = [result['url'] for sublist in organic_results for result in sublist]
+
+    # Create DataFrame
+    df = pd.DataFrame(urls, columns=['url'])
+
+    # Print the dataframe
+    print(df)
+    st.header("Scraped Data from SERP and SERP Links")
+    #st.write(df)
+    return df
+
